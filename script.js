e6a4bb554880ab01ea2a101b84cc1190bf9550ce
Abhaya <shankarabhaya9@gmail.com>
2022-11-11 00:43:28 +0530

    Adding list item using DOM via prompts

--- script.js
+++ script.js
@@ -6,29 +6,29 @@
 // createElement.style.color = "red"; //adding styles to the text
 
 //ABHAYA SHANKAR
-let olList = document.getElementById("ol");
-olList.before("before");
-olList.after("after");
+// let olList = document.getElementById("ol");
+// olList.before("before");
+// olList.after("after");
 
-let liFirst = document.createElement("li");
-liFirst.innerHTML = "prepend";
-olList.prepend(liFirst);
+// let liFirst = document.createElement("li");
+// liFirst.innerHTML = "prepend";
+// olList.prepend(liFirst);
 
-let liSecond = document.createElement("li");
-liSecond.innerHTML = "append";
-olList.append(liSecond);
+// let liSecond = document.createElement("li");
+// liSecond.innerHTML = "append";
+// olList.append(liSecond);
 
-//insertAdjacentHTML/Text/Element
+// //insertAdjacentHTML/Text/Element
 
-document.body.insertAdjacentHTML(
-  "afterbegin",
-  `<div class="alert">
-<strong>Hi there!</strong> You've read an important message.
-</div>`
-);
+// document.body.insertAdjacentHTML(
+//   "afterbegin",
+//   `<div class="alert">
+// <strong>Hi there!</strong> You've read an important message.
+// </div>`
+// );
 
-function clear(elem) {
-  setTimeout(() => elem.remove(), 1000); // it will remove the elem after 1sec of time.
-}
+// function clear(elem) {
+//   setTimeout(() => elem.remove(), 1000); // it will remove the elem after 1sec of time.
+// }
 
-clear(elem);
+// clear(elem);
