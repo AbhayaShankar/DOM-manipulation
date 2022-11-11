fe8b21a3d2217753bcb40616141f6aac70bc70b7
Abhaya <shankarabhaya9@gmail.com>
2022-11-11 12:20:08 +0530

    Classlist classsname and scroll

--- addingListUsingDOM.js
+++ addingListUsingDOM.js
@@ -1,15 +1,54 @@
 //adding list item via prompts using DOM. -- VERY IMPORTANT ASPECT
-let ul = document.createElement("ul");
-document.body.append(ul);
+// let ul = document.createElement("ul");
+// document.body.append(ul);
 
-while (true) {
-  var data = prompt("Enter a list Value :");
+// while (true) {
+//   var data = prompt("Enter a list Value :");
 
-  if (!data) {
-    break;
+//   if (!data) {
+//     break;
+//   }
+
+//   let listItem = document.createElement("li");
+//   listItem.textContent = data;
+//   ul.append(listItem);
+// }
+
+function showNotification({ top = 0, right = 0, className, html }) {
+  let notifictaion = document.createElement("div");
+  notifictaion.className = "notifictaion";
+
+  if (className) {
+    notifictaion.classList.add(className);
   }
 
-  let listItem = document.createElement("li");
-  listItem.textContent = data;
-  ul.append(listItem);
+  notifictaion.style.top = top + "px";
+  notifictaion.style.right = right + "px";
+  notifictaion.innerHTML = html;
+  document.body.append(notifictaion);
+  setTimeout(() => notifictaion.remove(), 1500);
 }
+
+// test it
+let i = 1;
+setInterval(() => {
+  showNotification({
+    top: 10,
+    right: 10,
+    html: "Hello " + i++,
+    className: "welcome",
+  });
+}, 2000);
+
+// SCROLL HEIGHT Of the Document.
+
+let scrollHeight = Math.max(
+  document.body.scrollHeight,
+  document.documentElement.scrollHeight,
+  document.body.offsetHeight,
+  document.documentElement.offsetHeight,
+  document.body.clientHeight,
+  document.documentElement.clientHeight
+);
+
+console.log(scrollHeight);
